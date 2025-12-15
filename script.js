680b174794417c9d89b68eb116c40caf1f896755
tusharnikam6789-boop <tusharnikam6789@gmail.com>
2025-12-15 23:16:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let name ="tushar";
+const age =20;
+
+function greet(user){
+    return  "hello  " + user;
+
+}
+const add =(a,b) => a+b;
+
+console.log(greet(name));
+console.log(add(5,4));
+
+document.getElementById("text").innerText ="Javascript is working !";
